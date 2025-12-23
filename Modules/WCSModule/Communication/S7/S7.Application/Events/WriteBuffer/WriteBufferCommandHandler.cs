1d7f618c9d2975452e22048fc6ffa6aa03db1ec4
吴 <@>
2025-12-23 15:38:24 +0800

    提交

---
+++ Modules/WCSModule/Communication/S7/S7.Application/Events/WriteBuffer/WriteBufferCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Common.Application.MediatR.Messaging;
+using Common.Domain;
+
+namespace S7.Application.Events.ReadBuffer;
+
+public class WriteBufferCommandHandler:ICommandHandler<WriteBufferCommand>
+{
+    public  Task<Result> Handle(WriteBufferCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}

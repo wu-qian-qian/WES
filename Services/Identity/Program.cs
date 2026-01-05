b951903c0defdd27a89dbbe5e4275e9544964dff
wuqi-bqkj <wuqi@bangqikeji.com>
2026-01-05 07:23:35 +0000

    commit

--- Services/Identity/Program.cs
+++ Services/Identity/Program.cs
@@ -1,3 +1,8 @@
 var builder = WebApplication.CreateSlimBuilder(args);
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwagger();
+}
 app.Run();
